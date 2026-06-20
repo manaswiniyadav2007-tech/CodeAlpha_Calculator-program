cdc0c475301c3a94f07c41d0d3eac630c57d1499
manaswiniyadav2007-tech <manaswiniyadav2007@gmail.com>
2026-06-20 21:31:52 +0530

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -1,6 +1,4 @@
-# CodeAlpha_Calculator-program
-A simple C Calculator Program built as part of the CodeAlpha Internship to perform basic arithmetic operations efficiently using switch-case and user interaction.
-#include <stdio.h>
+
 
 int main() {
     int choice;
